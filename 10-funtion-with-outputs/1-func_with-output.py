43f28350b9e0855f20a6a307ac0c193bf826a7f2
mansurio <mansurcodeio@gmail.com>
2023-01-10 17:05:12 +0700

    first commit

---
+++ 10-funtion-with-outputs/1-func_with-output.py
@@ -0,0 +1,23 @@
+# def format_name(f_name, l_name):
+#     fname = f_name.title()
+#     lname = l_name.title()
+#
+#
+#     return f"result : your {f_name} and {l_name}"
+#
+# print(format_name("Mansur", "saja"))
+# input("what is your first name...?")
+
+
+# ======================
+# ======================
+# ===DOCSTRING===
+# ======================
+
+def str():
+    out = """ hallo
+     ap kabar
+     name saya adahlah
+     """
+    print(out)
+str()
---
+++ 10-funtion-with-outputs/excercise-calculator.py
@@ -0,0 +1,27 @@
+# def add(n1, n2):
+#     return n1 + n2
+#
+#
+# def subtract(n1, n2):
+#     return n1 - n2
+#
+#
+# def multiply(n1, n2):
+#     return n1 * n2
+#
+#
+# def divide(n1, n2):
+#     return n1 / n2
+#
+#
+# print(add(2, multiply(5, divide(8, 4))))
+
+# def out_func(a, b):
+#     def in_fuc(c, d):
+#         return c + d
+#
+#     return in_fuc(a , b)
+#
+#
+# result = out_func(5, 10)
+# print(result)
