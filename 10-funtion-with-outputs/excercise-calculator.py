eb4c9e0a9630d365a1d9c1df6b0289b8c8ad120f
mansurio <mansurcodeio@gmail.com>
2023-01-10 18:22:28 +0700

    make calkulator part 1

--- 10-funtion-with-outputs/excercise-calculator.py
+++ 10-funtion-with-outputs/excercise-calculator.py
@@ -25,3 +25,37 @@
 #
 # result = out_func(5, 10)
 # print(result)
+
+# todo -- CALCULATOR --
+# ADD
+def add(n1, n2):
+    return n1 + n2
+
+
+# SUBSTRACK
+def substrac(n1, n2):
+    return n1 - n2
+
+
+# MULTIPLY
+def multyplay(n1, n2):
+    return n1 * n2
+
+
+# DIVIDE
+def divide(n1, n2):
+    return n1 / n2
+
+
+operation = {"+": add, "-": substrac, "*": multyplay, "/": divide}
+
+num1 = int(input("Whats the first number...?"))
+num2 = int(input("Whats the second number...?"))
+
+for simbol in operation:
+    print(simbol)
+
+ope_symbol = input("Pic the operator from the line above...:")
+calck_simbol = operation[ope_symbol]
+answer = calck_simbol(num1, num2)
+print(f" {num1} {ope_symbol} {num2} = {answer}")
